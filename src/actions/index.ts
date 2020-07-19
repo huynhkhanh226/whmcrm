7ceefef64d5707204c9b8e56b4614c67e8f771aa
raymond <Administrator@RAYMONDHUYNH.diginetcorp.com>
2020-07-19 20:04:05 +0700

    dfds

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,4 @@ export * from './packages';
 
 // https://host75.registrar-servers.com:2087/cpsess7173534241/json-api/createacct?api.version=1&username=raymond&domain=vndevops.com
 // https://host75.registrar-servers.com:2087/cpsess7173534241/json-api/listpkgs?api.version=1&want=all
+// https://host75.registrar-servers.com:2087/FHLPSIJTSQ0ZWZEFYFGTAQ1ZQMG17N67/json-api/listpkgs?api.version=1&want=all
